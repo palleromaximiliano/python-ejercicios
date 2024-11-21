496407e805c85c3a47c2d9b241205a62c9e62ebe
palleromaximiliano <argordroma@gmail.com>
2024-11-20 23:15:27 -0300

    primer commit

---
+++ practica 1.py
@@ -0,0 +1,6 @@
+'''aca declaramos variables de tipo int '''
+
+numero = 5
+numero_dos = 20
+resultdo = numero_dos + numero
+print(resultdo)
---
+++ practica 2.py
@@ -0,0 +1,9 @@
+'''aca vamos a sumar dos numeros pero antes vamos a tener q convertr el q esta en  string'''
+
+numero_string = "8"
+numero_comvertido_a_int = int(numero_string)
+numero_int = 55
+resultado = numero_comvertido_a_int + numero_int
+
+
+print(resultado)
